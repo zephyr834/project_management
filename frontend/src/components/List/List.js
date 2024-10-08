6a0e436b81dcd3b4e6809cdd64fad58b941dd01e
Cory Eighan <zephyr834@gmail.com>
2024-10-07 22:32:03 -0700

    In progress of Add and CardEditor

--- frontend/src/components/List/List.js
+++ frontend/src/components/List/List.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Card from "../Card/Card";
+import CardEditor from "../CardEditor";
+import shortid from "shortid";
 
 class List extends Component {
+    state = {
+        addingCard: false
+    };
+
+    toggleAddingCard = () =>
+        this.setState({ addingCard: !this.state.addingCard });
+
+    addCard = async cardText => {
+        const { listId, dispatch } = this.props;
+        this.toggleAddingCard();
+
+        const cardId = shortid.generate();
+
+        dispatch({
+            type: "ADD_CARD",
+            payload: { cardText, cardId, listId }
+        })
+    }
+
     render() {
         const { list } = this.props;
+        const { addingCard } = this.state;
 
         return (
             <div className="bg-c-white flex-shrink-0 w-[272px] h-fit m-2.5 mr-0 rounded-2.5 border border-black/10">
@@ -21,7 +43,19 @@ class List extends Component {
                         listId={list._id}
                     />
                 ))}
+                {addingCard ? (
+                    <CardEditor
+                        onSave={this.addCard}
+                        onCancel={this.toggleAddingCard}
+                        adding
+                    />
+                ) : (
+                    <div className="cursor-pointer p-2.5 text-gray-500 rounded-b-lg flex items-center hover:bg-[#092d4223] hover:text-[#17394d] hover:underline" onClick={this.toggleAddingCard} >
+                        <ion-icon name="add" className="mr-0.5" />Add Card
+                    </div>
+                )}
             </div>
+
         );
     }
 }
